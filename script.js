4b5f67c1dfea7d2f85b42ecbaa42a5f5e3bf46f9
Frank Reust <freust14@gmail.com>
2020-11-30 16:11:44 -0600

    form listener and validation

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+window.addEventListener("load", function () {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function (event) {
+      let pilotNameInput = document.querySelector("input[name=pilotName]");
+      let copilotNameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+      
+      if (!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value) ||
+      isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
+         alert("Make sure to enter valid information for each field!");
+         event.preventDefault();
+      } 
+
+      if (pilotNameInput.value === "" || copilotNameInput.value === "" || 
+         fuelLevelInput.value === "" || cargoMassInput.value === "") {
+         alert("All fields are required!");
+			event.preventDefault();
+      }
+
+      
+
+
+
+      
+
+   });
+   
+ 
+
+   
+});
+
 // Write your JavaScript code here!
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
